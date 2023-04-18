3b5cbe6dd466c4b99c3eff001526ad2ab814e0d4
«Elena <snogotok@gmail.com>
2023-04-18 16:04:21 +0300

    task 1

---
+++ 1/1.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+
+// Вам даны два внутренних угла (в градусах) треугольника.
+// 
+// Напишите функцию, возвращающую 3-е число.
+// 
+// Примечание: будут проверены только положительные целые числа.
+// 
+// https://en.wikipedia.org/wiki/Треугольник
+
+function otherAngle(a, b) {
+    return 180 - a - b;
+  }
+
+  console.log(otherAngle(60, 60));
