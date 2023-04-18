d55cd703b90b54bec0628faafa2b7b10c731238c
LenaRybinskova@gmail.com <LenaRybinskova@gmail.com>
2023-04-18 16:46:30 +0300

    1

--- 1/1.js
+++ 1/1.js
@@ -9,7 +9,7 @@
 // https://en.wikipedia.org/wiki/Треугольник
 
 function otherAngle(a, b) {
-    return 180 - a - b;
-  }
+  return 180 - a - b;
+}
 
-  console.log(otherAngle(60, 60));
+console.log(otherAngle(60, 60));
