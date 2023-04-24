d1ee19c9a992716e07c4335b6e59b81ee42dafd8
Elena <LenaRybinskova@gmail.com>
2023-04-24 16:47:09 +0300

    9

--- 8/index.js
+++ 8/index.js
@@ -1,3 +1,14 @@
 function multiply(a, b){
     return a * b
   }
+
+  var summation = function (num) {
+    res=0
+    for(let i=0;i<=num; i++){
+        res=res+i
+    }
+    return res
+  }
+
+  console.log(summation(8))
+
