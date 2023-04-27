a15c48409bebbe5d5d85006a36071b176379518e
Elena <LenaRybinskova@gmail.com>
2023-04-27 09:36:41 +0300

    33

--- 8/index.js
+++ 8/index.js
@@ -217,3 +217,31 @@ function greet(name){
     return `Hello, ${name} how are you doing today?`
   }
 //  console.log(greet("Ryan")) 
+
+
+
+function getGrade (s1, s2, s3) {
+let res=((s1+s2+s3)/3)
+if(res>=90 && res<=100){
+  return 'A'
+} 
+else if(res>=80 && res<90){
+  return 'B'
+}
+else if(res>=70 && res<80){
+  return 'C'
+}
+else if(res>=60 && res<70){
+  return 'D'
+}
+else if(res>=0 && res<60){
+  return 'F'
+}
+}
+// console.log(getGrade(95,90,93))
+
+
+
+
+
+
