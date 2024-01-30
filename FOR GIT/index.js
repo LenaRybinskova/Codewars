1c376519126e349aefc53ab224ce9d29ef7f7eb0
Elena <LenaRybinskova@gmail.com>
2024-01-30 18:24:03 +0300

    90

--- FOR GIT/index.js
+++ FOR GIT/index.js
@@ -846,3 +846,8 @@ function getAverage(marks) {
 const areaOrPerimeter = function (l, w) {
     return l === w ? l *w : (l + w) * 2
 };
+
+
+function removeExclamationMarks(s) {
+    return [...s].filter(el=>el!="!").join("")
+}
